dbe8799c443908d64e8370f6751ff64059f74268
feldway1 <feldway120@gmail.com>
2024-06-17 15:58:05 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace CSharp_Laivay1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txttien = new System.Windows.Forms.TextBox();
+            this.txtlai = new System.Windows.Forms.TextBox();
+            this.txtnam = new System.Windows.Forms.TextBox();
+            this.lbxlai = new System.Windows.Forms.ListBox();
+            this.btntinh = new System.Windows.Forms.Button();
+            this.btnthoat = new System.Windows.Forms.Button();
+            this.btnhuy = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(180, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(303, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "LÃI SUẤT VAY NGÂN HÀNG";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(48, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Số tiền";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(48, 161);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 23);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Lãi suất";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(48, 221);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(107, 23);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Số năm gửi";
+            // 
+            // txttien
+            // 
+            this.txttien.Location = new System.Drawing.Point(171, 93);
+            this.txttien.Name = "txttien";
+            this.txttien.Size = new System.Drawing.Size(100, 30);
+            this.txttien.TabIndex = 4;
+            // 
+            // txtlai
+            // 
+            this.txtlai.Location = new System.Drawing.Point(171, 154);
+            this.txtlai.Name = "txtlai";
+            this.txtlai.Size = new System.Drawing.Size(100, 30);
+            this.txtlai.TabIndex = 5;
+            // 
+            // txtnam
+            // 
+            this.txtnam.Location = new System.Drawing.Point(171, 214);
+            this.txtnam.Name = "txtnam";
+            this.txtnam.Size = new System.Drawing.Size(100, 30);
+            this.txtnam.TabIndex = 6;
+            // 
+            // lbxlai
+            // 
+            this.lbxlai.FormattingEnabled = true;
+            this.lbxlai.ItemHeight = 23;
+            this.lbxlai.Location = new System.Drawing.Point(70, 328);
+            this.lbxlai.Name = "lbxlai";
+            this.lbxlai.Size = new System.Drawing.Size(391, 142);
+            this.lbxlai.TabIndex = 7;
+            // 
+            // btntinh
+            // 
+            this.btntinh.Location = new System.Drawing.Point(385, 214);
+            this.btntinh.Name = "btntinh";
+            this.btntinh.Size = new System.Drawing.Size(109, 31);
+            this.btntinh.TabIndex = 8;
+            this.btntinh.Text = "Tính";
+            this.btntinh.UseVisualStyleBackColor = true;
+            this.btntinh.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnthoat
+            // 
+            this.btnthoat.Location = new System.Drawing.Point(482, 435);
+            this.btnthoat.Name = "btnthoat";
+            this.btnthoat.Size = new System.Drawing.Size(91, 34);
+            this.btnthoat.TabIndex = 9;
+            this.btnthoat.Text = "Thoát";
+            this.btnthoat.UseVisualStyleBackColor = true;
+            this.btnthoat.Click += new System.EventHandler(this.btnthoat_Click);
+            // 
+            // btnhuy
+            // 
+            this.btnhuy.Location = new System.Drawing.Point(519, 214);
+            this.btnhuy.Name = "btnhuy";
+            this.btnhuy.Size = new System.Drawing.Size(81, 30);
+            this.btnhuy.TabIndex = 10;
+            this.btnhuy.Text = "Hủy";
+            this.btnhuy.UseVisualStyleBackColor = true;
+            this.btnhuy.Click += new System.EventHandler(this.btnhuy_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(641, 506);
+            this.Controls.Add(this.btnhuy);
+            this.Controls.Add(this.btnthoat);
+            this.Controls.Add(this.btntinh);
+            this.Controls.Add(this.lbxlai);
+            this.Controls.Add(this.txtnam);
+            this.Controls.Add(this.txtlai);
+            this.Controls.Add(this.txttien);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "Form1";
+            this.Text = "LÃI SUẤT VAY NGÂN HÀNG";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txttien;
+        private System.Windows.Forms.TextBox txtlai;
+        private System.Windows.Forms.TextBox txtnam;
+        private System.Windows.Forms.ListBox lbxlai;
+        private System.Windows.Forms.Button btntinh;
+        private System.Windows.Forms.Button btnthoat;
+        private System.Windows.Forms.Button btnhuy;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharp_Laivay1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lbxlai.Items.Clear();
+            double tienlai;
+            double tien = double.Parse(txttien.Text);
+            double laisuat = double.Parse(txtlai.Text.Replace("%",""))/100;
+            int sonam = int.Parse(txtnam.Text);
+            for (int i = 1 ; i<= sonam;i++)
+            {
+                tienlai = tien * laisuat;
+                lbxlai.Items.Add($"{i}\t {tienlai:F2}\t {tienlai:F2}");
+                tien += tienlai;
+                
+            }
+            
+        }
+
+        private void btnthoat_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnhuy_Click(object sender, EventArgs e)
+        {
+            txttien.ResetText();
+            txtlai.ResetText();
+            txtnam.ResetText();
+            lbxlai.Items.Clear();
+        }
+    }
+}
